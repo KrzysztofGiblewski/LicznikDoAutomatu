2faf6a5cf7925db53dc651b12022b5214ceeced8
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-07-05 11:31:51 +0200

    dodane dwa przyciski dodaje i odejmuje

--- licznik.ino
+++ licznik.ino
@@ -5,12 +5,13 @@
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-   int ilePaczka=0;
-   int ileKarton=0;
-   int ileWszys=0;
+   int ilePacz=0;
+   int ileKart=0;
+   int ileWszy=0;
+   
 void setup() {
   lcd.begin(16, 2);
-  lcd.print("LICZNIK");
+  lcd.print("0");
   pinMode(14, INPUT_PULLUP); 
   pinMode(15, INPUT_PULLUP); 
    pinMode(16, INPUT_PULLUP); 
@@ -18,10 +19,24 @@ void setup() {
 }
 
 void loop() {
+  if (digitalRead(14) == LOW)
+   {
+      delay(300);
+      ilePacz++;
+      ileKart++;
+      ileWszy++;
+   }
+     if (digitalRead(15) == LOW)
+   {
+      delay(300);
+      ilePacz--;
+      ileKart--;
+      ileWszy--;
+   }
   lcd.setCursor(0,0);
-    lcd.print(ileWszys);
+    lcd.print(ileWszy);
          lcd.setCursor(0, 1);
-         lcd.print(ilePaczka);
+         lcd.print(ilePacz);
          
        
   
